94c9c3ef1646337e70487955d14b5f5d8fa7f1b2
kingDaniel2004 <danielelvisr@gmail.com>
2021-05-07 16:18:09 -0400

    added project

---
+++ src/App.java
@@ -0,0 +1,12 @@
+public class App {
+
+    App(){
+        for (int i = 0; i < 100; i++){
+            System.out.println(i + 1);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        new App();
+    }
+}
