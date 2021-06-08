1aa9cda72c0612c95c66bc655ec1d157c0934903
kingDaniel2004 <danielelvisr@gmail.com>
2021-06-08 18:25:20 -0400

    added more

--- Chess/src/GUI/UserClick.java
+++ Chess/src/GUI/UserClick.java
@@ -6,56 +6,60 @@ import Assets.*;
 class UserClick implements ActionListener {
     static boolean whiteTurn = true;
     static boolean showLegalMove = true;
-    Tile button;
-    private Piece prev_piece, piece;
-    private PieceColor pieceColor, prev_pieceColor;
+    private Tile button;
+    private Piece focused_piece;
+    private PieceColor pieceColor;
     private TileColor tileColor;
-    int x, y;
+    int tile_x, tile_y;
 
     @Override
     public void actionPerformed(ActionEvent e) {
         this.button = (Tile) e.getSource();
         this.tileColor = button.getTileColor();
-        this.piece = button.getPiece();
-        this.x = button.getTile_x();
-        this.y = button.getTile_y();
+        this.tile_x = button.getTile_x();
+        this.tile_y = button.getTile_y();
 
+        moveOrShowLegalMove();
+    }
+
+    /**
+     * shows legal moves or move the piece to its legal clicken sport
+     */
+    public void moveOrShowLegalMove(){
         if (Board.getFocusedPiece() != null){
             if (tileColor.equals(TileColor.LEGAL_DARK) || tileColor.equals(TileColor.LEGAL_LIGHT)){
-                this.prev_piece = Board.getFocusedPiece();
+                this.focused_piece = Board.getFocusedPiece();
                 this.pieceColor = Board.getFocusedPiece().getColor();
                 move();
             }
         } else {
-                showLegalMove();
+            showLegalMove();
         }
-
-
-
-
-       if (button.getPiece() == null) return;
-
-        // Board.setPreviousPiece(button.getPiece());
     }
 
     
 
+    /**
+     * show legal moves of the clicked piece by changing the tile color.
+     */
     public void showLegalMove(){
         if (button.getPiece() == null) return;
 
         if ( whiteTurn && button.getPiece().getColor().equals(PieceColor.BLACK)) return;
         if (!whiteTurn && button.getPiece().getColor().equals(PieceColor.WHITE)) return;
 
-        changeTileColor(x, y);
+        changeTileColor(tile_x, tile_y);
 
         if (whiteTurn){ 
             for (int i = 0; i < Board.tile.length; i++) 
                 for (int j = 0; j < Board.tile[i].length; j++) 
                     changeTileColor(i, j);
+
         } else {
             for (int i = 0; i < Board.tile.length; i++) 
                 for (int j = 0; j < Board.tile[i].length; j++) 
                     changeTileColor(i, j);
+
         }
 
 
@@ -63,6 +67,11 @@ class UserClick implements ActionListener {
         Board.setFocusedPiece(button.getPiece());
     }
 
+    /**
+     * highlight the color of the tile which the clicked piece can move.
+     * @param x x location of the tile
+     * @param y y location of the tile
+     */
     static void changeTileColor(int x, int y){
         if (Board.tile[x][y].getTileColor().equals(TileColor.DARK))
             Board.tile[x][y].setTileColor(TileColor.LEGAL_DARK);
@@ -71,21 +80,15 @@ class UserClick implements ActionListener {
     }
 
 
+    /**
+     * move the chess piece to the clicked highlighted tile from its current tile.
+     */
     public void move(){
-        // print();
-        // System.out.println("pre_piececolor: " + pieceColor);
-        // if      ( whiteTurn && pieceColor.equals(PieceColor.WHITE)) moveWhite();
-        // else if (!whiteTurn && pieceColor.equals(PieceColor.BLACK)) moveBlack();
-        int x1 = prev_piece.getX(), y1 = prev_piece.getY();
-        int x2 = this.x, y2 = this.y;
+        int x1 = focused_piece.getX(), y1 = focused_piece.getY();
+        int x2 = this.tile_x, y2 = this.tile_y;
 
-        // System.out.println("x1: " + prev_piece.getX() + " | y1: " + prev_piece.getY());
-        // System.out.println("x1: " + x + " | y1: " + y);
-
-        if (tileColor.equals(TileColor.LEGAL_DARK) || tileColor.equals(TileColor.LEGAL_LIGHT)){
-            prev_piece.move(x, y);
-        }
-
+        if (tileColor.equals(TileColor.LEGAL_DARK) || tileColor.equals(TileColor.LEGAL_LIGHT))
+            focused_piece.move(tile_x, tile_y);
 
         Board.printPieces();
         Board.setFocusedPiece(null);
@@ -96,13 +99,19 @@ class UserClick implements ActionListener {
     void print(){
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \n");
         System.out.println("whiteturn: " + whiteTurn);
-        System.out.println("prv_piece: " + prev_piece.getClass().getSimpleName());
+        System.out.println("prv_piece: " + focused_piece.getClass().getSimpleName());
         System.out.println("prev_color: " + pieceColor);
         System.out.println("\n\n");
 
     }
 
-    public Piece getPiece() { return prev_piece; }
+    /**
+     * get the focued piece (one which is ready to move in next valid click)
+     * @return Piece
+     */
+    public Piece getFocusedPiece() { 
+        return focused_piece; 
+    }
 
 
 
